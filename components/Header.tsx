fe5ea9d6224f75c730b5f97a06142babf054a081
Javier Ocampo <info@javierocampo.net>
2022-05-25 15:26:32 +0200

    feat(create-header): add search and menu icons

---
+++ components/Header.tsx
@@ -0,0 +1,73 @@
+import React from 'react';
+import Image from 'next/image';
+import { signIn, signOut, useSession } from 'next-auth/react';
+import { MenuIcon , HomeIcon, ChevronDownIcon, SearchIcon } from '@heroicons/react/solid';
+import { BellIcon, ChatIcon, GlobeIcon, PlusIcon, SparklesIcon, SpeakerphoneIcon, VideoCameraIcon  } from '@heroicons/react/outline';
+import imgfqy from '../public/images/fqy.webp';
+import img23l from '../public/images/23l.webp';
+
+const Header: React.FC<{}> = () => {
+  const { data: session } = useSession();
+  return (
+    <div className='sticky top-0 z-50 flex bg-white px-4 py-2 shadow-sm'>
+      <div className='relative h-10 w-20 flex-shrink-0 cursor-pointer'>
+        <Image
+          objectFit='contain'
+          src={imgfqy}
+          layout='fill'
+        />
+      </div>
+      <div className='mx-7 flex items-center xl:min-w-[300px]'>
+        <HomeIcon className='h-5 w-5' />
+        <p className='ml-2 hidden flex-1 lg:inline'>Home</p>
+        <ChevronDownIcon className='h-5 w-5' />
+      </div>
+      <form className='flex flex-1 items-center space-x-2 rounded-sm border border-gray-200 bg-gray-100 px-3 py-1'>
+        <SearchIcon className='h-6 w-6 text-gray-400' />
+        <input className='flex-1 bg-transparent outline-none' type='text' placeholder='Search Reddit' />
+        <button type='submit' hidden />
+      </form>
+      <div className='mx-5 hidden items-center space-x-2 text-gray-500 lg:inline-flex'>
+        <SparklesIcon className='icon' />
+        <GlobeIcon className='icon' />
+        <VideoCameraIcon className='icon' />
+        <hr className='h-10 border border-gray-100' />
+        <ChatIcon className='icon' />
+        <BellIcon className='icon' />
+        <PlusIcon className='icon' />
+        <SpeakerphoneIcon className='icon' />
+      </div>
+      <div className='ml-5 flex items-center lg:hidden'>
+        <MenuIcon className='icon' />
+      </div>
+      {session ? (
+        <div
+        onClick={() => signOut()}
+        className='hidden curso-pointer items-center space-x-2 border border-gray-200 p-2 lg:flex'
+      >
+        <div className='relative h-5 w-5 flex-shrink-0'>
+          <Image objectFit='contain' src={img23l} layout='fill' alt='' />
+        </div>
+        <div className='flex-1 text-xs'>
+          <p className='truncate'>{session?.user?.name}</p>
+          <p className='text-gray-400'>1 Karma</p>
+        </div>
+        <ChevronDownIcon className='h-5 flex-shrink-0 text-gray-400' />
+      </div>
+      ) : (
+        <div
+        onClick={() => signIn()}
+        className='hidden curso-pointer items-center space-x-2 border border-gray-200 p-2 lg:flex'
+      >
+        <div className='relative h-5 w-5 flex-shrink-0'>
+          <Image objectFit='contain' src={img23l} layout='fill' alt='' />
+        </div>
+        <p className='text-gray-400'>Sing In</p>
+      </div>
+      )}
+      
+    </div>
+  )
+}
+
+export default Header;
